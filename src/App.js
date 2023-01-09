fca9954468968c49eb5b0b86e70b5376463c629b
Rahul kumar <rahulpatel14042002@gmail.com>
2023-01-09 17:43:21 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home'
 import Login from './components/Login'
 
